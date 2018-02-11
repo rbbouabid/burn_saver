b17124666536b92bdeeb51ca2c6801babdee63b2
Ryan Bouabid <ryan-bouabid@live.com>
2018-02-12 00:16:14 +0100

    cleaned up some of the modular stuff

--- ana.py
+++ ana.py
@@ -1,9 +1,19 @@
 import sys
+from peakfinder import finder
 from burnchecker import burncheck
-
 
 print "this is the top level analysis macro"
 print "i call the peakfinder and feed the info to the burnchecker"
+
+partition_list = ["EBA", "EBC", "LBA", "LBC"]
+module_list = range(65)
+module = "42"
+
+for i in range(len(partition_list)):
+	if partition_list[i] != "EBA": continue
+	for j in range(len(module_list)):
+		if module_list[j] != 42: continue
+		finder(partition_list[i], str(module_list[j]))
 
 burncheck()
 
--- peakfinder.py
+++ peakfinder.py
@@ -7,6 +7,7 @@ print "i find peaks"
 container_size = 3 
 I_container = np.zeros(container_size)
 window = timedelta(1)
+sd = 3
 
 
 def get_datetime(string1, string2):
@@ -16,41 +17,49 @@ def get_datetime(string1, string2):
 	return datetime.strptime(time_string, '%d-%m-%Y %H:%M:%S')
 	
 
-try:
-	with open("data/EBA_LVPS_42_5VMB_OUTPUT_I.txt") as f:
-		fdata = [line.rstrip() for line in f]
-		for i,l in enumerate(fdata):
-			if(i < 100):
-				continue
+def threshold_alg(currents, threshold):
+	print "gonna calculate some thresholds >:(" 
 
-			# grabbing the datetime obj
-			values = l.split()
-			pt_time = get_datetime(values[1], values[2])
 
-			## grabbing the current in amps
-			pt_amps = float(values[0].strip("0"))
-
-			# some variables to loop backward from this pt
-			n = 1
-			delta_t = timedelta(0) 
-			prev_amps = []
-
-			while delta_t < window:
-				# grabbing (i-n)th line
-				prev_line = fdata[i-n]
-				prev_values = prev_line.split()
-				# (i-n)th time
-				prev_time = get_datetime(prev_values[1], prev_values[2])
-				delta_t = pt_time - prev_time
-				# pushing to container of previous I(amps)
-				prev_amps.append(float(prev_values[0].strip("0")))
-
-				# incrementing to next line
-				n += 1
-				
-			print "previous days worth of currents: ", prev_amps
-			print "\n"
+def finder(partition, module):
+	file_string = "data/" + partition + "_LVPS_" + module + "_5VMB_OUTPUT_I.txt"
+	try:
+		#with open("data/EBA_LVPS_42_5VMB_OUTPUT_I.txt") as f:
+		with open(file_string) as f:
+			fdata = [line.rstrip() for line in f]
+			for i,l in enumerate(fdata):
+				if(i < 100):
+					continue
+	
+				# grabbing the datetime obj
+				values = l.split()
+				pt_time = get_datetime(values[1], values[2])
+	
+				## grabbing the current in amps
+				pt_amps = float(values[0].strip("0"))
+	
+				# some variables to loop backward from this pt
+				n = 1
+				delta_t = timedelta(0) 
+				prev_amps = []
+	
+				while delta_t < window:
+					# grabbing (i-n)th line
+					prev_line = fdata[i-n]
+					prev_values = prev_line.split()
+					# (i-n)th time
+					prev_time = get_datetime(prev_values[1], prev_values[2])
+					delta_t = pt_time - prev_time
+					# pushing to container of previous I(amps)
+					prev_amps.append(float(prev_values[0].strip("0")))
+	
+					# incrementing to next line
+					n += 1
 					
-
-except IOError:
-	print "Whoa there! You're trying to open a file you do not have..\n"
+				print "previous days worth of currents: ", prev_amps
+				threshold_alg(prev_amps, sd)
+				print "\n"
+						
+	
+	except IOError:
+		print "Whoa there! You're trying to open a file you do not have..\n"
